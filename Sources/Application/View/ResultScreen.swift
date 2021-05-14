a136830bb8dad073158b4a648ef129b7b2cb6dcd
hayabusabusa <hayabusabusaSY@gmail.com>
2021-05-15 00:21:02 +0900

    ui: fix result screen layout

--- Sources/Application/View/ResultScreen.swift
+++ Sources/Application/View/ResultScreen.swift
@@ -15,20 +15,27 @@ public struct ResultScreen: View {
         NavigationView {
             ScrollView {
                 LazyVStack(spacing: 0) {
+                    // MARK: Header
                     ResultHeader(percent: "100", percentColor: .green, numberOfQuiz: 10, numberOfCorrect: 10, action: {})
                         .background(Color(UIColor.systemBackground))
+                    
+                    // MARK: Separator
                     ResultSeparator(title: "問題一覧", showLine: false)
+                    
+                    // MARK: Cells
                     ForEach(0 ..< 10) { index in
                         ResultCell(title: "This is TEST data. Index \(index)", answer: "index \(index)", isCorrect: index % 2 == 0)
                             .background(Color(UIColor.systemBackground))
                     }
                 }
+                // NOTE: 一応画面下のインセットを追加
+                .padding(.bottom, 16)
             }
             .background(Color(UIColor.systemGroupedBackground))
+            .edgesIgnoringSafeArea(.bottom)
             .navigationTitle("結果")
             .navigationBarTitleDisplayMode(.inline)
         }
-        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
